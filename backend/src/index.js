624a885fe9f8aca96bb9920a0f82cfcc887e84a9
lucaslombardif <lucaas-floriano@hotmail.com>
2020-04-17 00:43:03 -0300

    Ajustes p/ deploy #1

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,15 +13,13 @@ mongoose.connect('mongodb+srv://lucas:caslu@cluster0-g0ejr.mongodb.net/test?retr
   useUnifiedTopology: true
 });
 
-app.use(cors());
-
 app.use((req, res, next) => {
   req.io = io;
 
   next();
 });
 
+app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
-
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
